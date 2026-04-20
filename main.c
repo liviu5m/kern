88584200aac771ac6897f7e982fcba118a6cb75f
liviu <motpanliviuwork@gmail.com>
2026-04-20 18:21:13 +0300

    Done pipelining and history

--- main.c
+++ main.c
@@ -27,7 +27,8 @@ char *builtinCmds[] = {
   "exit",
   "echo",
   "type",
-  "jobs"
+  "jobs",
+  "history"
 };
 char *pathValues[BUFFER_SIZE];
 int pathCount = 0;
@@ -92,11 +93,6 @@ int execute(char *args[],int ind, char * command) {
       printf("[%d] %d \n", jobsCount, jobId);
 
       return 0;
-    }else {
-      if(execvp(args[0], args) == -1) {
-        perror("execvp");
-        _exit(1);
-      }
     }
   }
 
@@ -127,19 +123,19 @@ int execute(char *args[],int ind, char * command) {
     }
   }
 
-  if(strcmp(command, "exit") == 0) {
+  if(strcmp(args[0], "exit") == 0) {
     return -1;
-  }else if(strncmp(command, "echo ", 5) == 0) {
+  }else if(strcmp(args[0], "echo") == 0) {
     for(int i = 1;i<ind;i++) {
       printf("%s", args[i]);
       if(i != ind-1) printf(" ");
       else printf("\n");
     }
-  }else if(strncmp(command, "type ", 5) == 0) {
+  }else if(strcmp(args[0], "type") == 0) {
     int val = isBuiltinCommand(args[1]);
     if(val == 0) printf("%s: not found\n", args[1]);
     return 0;
-  }else if(strcmp(command, "pwd") == 0) {
+  }else if(strcmp(args[0], "pwd") == 0) {
     printf("%s\n", path);
   }else if(strcmp(args[0], "cd") == 0) {
     if(args[1][0] == '~') {
@@ -166,6 +162,24 @@ int execute(char *args[],int ind, char * command) {
     for(int i = 0;i<jobsCount;i++) {
       printf("[%d]  + Running       %s\n", jobs[i].id + 1, jobs[i].command);
     }
+  }else if(strcmp(args[0], "history")==0) {
+    HIST_ENTRY **the_list = history_list();
+
+    if (the_list) {
+      if(ind > 1) {
+        int totalItems = 0;
+        while (the_list[totalItems]) {
+          totalItems++;
+        }
+        for (int i = totalItems-1; i>=totalItems-atoi(args[1]); i--) {
+          printf("%d  %s\n", i + history_base, the_list[i]->line);
+        }
+      }else {
+        for (int i = 0; the_list[i]; i++) {
+          printf("%d  %s\n", i + history_base, the_list[i]->line);
+        }
+      }
+    }
   }else {
     pid_t pid = fork();
     if(pid < 0) {
@@ -188,6 +202,71 @@ int execute(char *args[],int ind, char * command) {
     exit(EXIT_SUCCESS);
   }
 }
+
+int pipelineCheck(char *args[], int argsCount, char command[]) {
+  int pipefds[2], input_fd = 0;
+  int num_pipes = 0;
+  int status = 0;
+  for(int i = 0; i < argsCount; i++) if(strcmp(args[i], "|") == 0) num_pipes++;
+  if (num_pipes == 0) {
+    status = execute(args, argsCount, command);
+  } else {
+    int start = 0;
+    for (int i = 0; i <= num_pipes; i++) {
+      int end = start;
+      while (end < argsCount && strcmp(args[end], "|") != 0) end++;
+      char *current_cmd_args[64];
+      int curr_count = 0;
+      for (int j = start; j < end; j++) current_cmd_args[curr_count++] = args[j];
+      current_cmd_args[curr_count] = NULL;
+
+      if (i < num_pipes) pipe(pipefds);
+
+      pid_t pid = fork();
+      if (pid == 0) {
+        if (i > 0) { dup2(input_fd, 0); close(input_fd); }
+        if (i < num_pipes) { close(pipefds[0]); dup2(pipefds[1], 1); close(pipefds[1]); }
+        status = execute(current_cmd_args, curr_count, current_cmd_args[0]); 
+        exit(0);
+      } else {
+        if (i > 0) close(input_fd);
+        if (i < num_pipes) { close(pipefds[1]); input_fd = pipefds[0]; }
+        start = end + 1;
+      }
+    }
+    for (int i = 0; i <= num_pipes; i++) wait(NULL);
+  }
+  return status;
+}
+
+
+int commands(char command[]) {
+  char *args[1024];
+  int ind = parseCommand(command, args);
+  if(ind == 0) return 0;
+  
+  int startI = 0;
+  char *perArgs[1024];
+  int argsCount = 0;
+  int status = 0;
+  while(startI<ind) {
+    for(int i = startI;i<ind;i++) {
+      if(strcmp(args[i], "&&") == 0) break;
+      else {
+        perArgs[argsCount++] = args[i];
+      }
+    }
+
+    status = pipelineCheck(perArgs, argsCount, command);
+
+    if(status == -1) return 0;
+    startI += argsCount + 1;
+    argsCount = 0;
+    perArgs[0] = '\0';
+  }
+  return 0;
+}
+
 
 void removeIndexFromArray(int ind) {
   for (int i = ind; i < jobsCount - 1; i++) {
@@ -218,31 +297,6 @@ void handle_sigchld(int sig) {
   }
 }
 
-int commands(char command[]) {
-  char *args[1024];
-  int ind = parseCommand(command, args);
-  if(ind == 0) return 0;
-  
-  int startI = 0;
-  char *perArgs[1024];
-  int argsCount = 0;
-  while(startI<ind) {
-    for(int i = startI;i<ind;i++) {
-      if(strcmp(args[i], "&&") == 0) break;
-      else {
-        perArgs[argsCount++] = args[i];
-      }
-    }
-    int status = execute(perArgs, argsCount, command);
-    if(status == -1) return 0;
-    startI += argsCount + 1;
-    argsCount = 0;
-    perArgs[0] = '\0';
-  }
-  return 0;
-}
-
-
 
 int detectSpace() {
   int c = 0;
@@ -264,12 +318,27 @@ int handleTabCompletion() {
   return 0;
 }
 
+void initialize_history(const char *path) {
+  if (access(path, F_OK) == -1) {
+    int fd = open(path, O_CREAT | O_WRONLY, 0600);
+    if (fd != -1) {
+      close(fd);
+    }
+  }
+
+  read_history(path);
+}
+
 int main()
 {
+  homePath = getenv("HOME");
+  char historyPath[1024];
+  snprintf(historyPath, sizeof(historyPath), "%s/.kern_shell_history", homePath);
+  initialize_history(historyPath);
+  stifle_history(500);
   signal(SIGCHLD, handle_sigchld);
   rl_bind_key('\t', handleTabCompletion);
   rl_event_hook = detectSpace;
-  homePath = getenv("HOME");
   if(homePath == NULL) homePath = "/";
   currentDirectory = malloc(BUFFER_SIZE);
   getPwd(currentDirectory);
@@ -300,6 +369,8 @@ int main()
     if(command[0] == '\n') {
       continue;
     }
+    add_history(command);
+    append_history(1, historyPath);
     int val = commands(command);
     free(command);
     if(val == -1) break;
